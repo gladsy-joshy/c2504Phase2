d40c427fdfcc258f6bf163e1cc15d4845f22a690
gladsy-joshy <161554567+gladsy-joshy@users.noreply.github.com>
2024-10-02 17:02:36 +0530

    02-10-2024

---
+++ Example14BrushesProject/Example14BrushesProject/FormConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Example14BrushesProject
+{
+    static class FormConfig
+    {
+        public static Window FrmSolid { get; set; } = new SolidColor();
+        public static Window FrmLinearGradient { get; set; } = new LinearGradient();
+
+        public static Window FrmRadialGradient { get; set; } = new RadialGradient();
+
+        public static Window FrmImage { get; set; } = new Image();
+
+        public static Window FrmVisual { get; set; } = new Visual();
+
+        public static Window FrmStack { get; set; } = new StackWindow();
+        public static Window FrmWrap { get; set; } = new WrapWindow();
+        public static Window FrmCanvas { get; set; } = new CanvasWindow();
+        public static Window FrmGrid { get; set; } = new GridWindow();
+        public static Window FrmDock { get; set; } = new DockWindow();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Example14BrushesProject/Example14BrushesProject/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example14BrushesProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //FormConfig.FrmSolid.Show();
+            FormConfig.FrmSolid.ShowDialog();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmLinearGradient.Show();
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmRadialGradient.Show();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmImage.Show();
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmVisual.Show();
+
+           
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmStack.Show();
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmWrap.Show();
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmCanvas.Show();
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmGrid.Show();
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmDock.Show();
+
+        }
+    }
+}
